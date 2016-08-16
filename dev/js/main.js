d738a0b4f4ae0016cff569c059f0d2f8016b533f
Doron <me@doronorenstein.com>
2016-08-15 22:11:37 -0500

    added functionality to redirect user to exact purchase on infinitemusician.com

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,6 +1,6 @@
-jQuery(document).ready(function() {
-    jQuery('a.buy-btn').on('click', function(e) {
+jQuery(document).ready(function($) {
+    $('a.buy-btn').on('click', function(e) {
         e.preventDefault();
-        console.log(jQuery('select#product_options').val());
+        window.location.href='http://www.infinitemusician.com/shop/saxophone-education/bulletproof-saxophone-playing/?add-to-cart=' + $('select#product_options').val(); 
     });
 });
